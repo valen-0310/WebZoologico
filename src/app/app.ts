668fdeda6ece7c122af21b4373523d6633ce792e
Valentina_Tangarife <159496242+valen-0310@users.noreply.github.com>
2026-04-16 20:13:30 -0500

    Add files via upload

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('WebZoologico');
+  alias="valen";
+}
